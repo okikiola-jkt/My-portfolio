ec6b041423edb25758276d760ed61fff869a1586
Okikiola Ojikutu <okikiolaojikutue@gmail.com>
2023-08-22 11:51:02 -0600

    New features

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import {FaBars, FaTimes, FaGithub, FaLinkedin, FaFacebook} from 'react-icons/fa'
 import {HiOutlineMail} from 'react-icons/hi'
 import {BsFillPersonLinesFill} from 'react-icons/bs'
 import Logo from '../assets/logo.png'
+import {Link} from 'react-scroll'
 
 const Navbar = () => {
   const [nav, setNav] = useState(false)
@@ -14,29 +15,69 @@ const Navbar = () => {
         <img src={Logo} alt="Logo Image" style={{width: '135px'}}/>
       </div>
         <ul className='hidden md:flex'>
-          <li>Home</li>
-          <li>About</li>
-          <li>Experience</li>
-          <li>Projects</li>
-          <li>Contact</li>
+          <li>
+            <Link to="home" smooth={true} duration={500}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="about" smooth={true} duration={500}>
+                About
+            </Link>
+          </li>
+          <li>
+          <Link to="skills" smooth={true} duration={500}>
+                Skills
+          </Link>
+          </li>
+          <li>
+            <Link to="projects" smooth={true} duration={500}>
+              Projects
+            </Link>
+          </li>
+          <li>
+          <Link to="contact" smooth={true} duration={500}>
+              Contact Me
+          </Link>
+          </li>
         </ul>
       <div onClick={handleClick} className='md:hidden z-10'>
         {!nav ? <FaBars /> : <FaTimes />}
       </div>
       {/* mobile menu */}
       <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-          <li className='py-6 text-4xl'>Home</li>
-          <li className='py-6 text-4xl'>About</li>
-          <li className='py-6 text-4xl'>Experience</li>
-          <li className='py-6 text-4xl'>Projects</li>
-          <li className='py-6 text-4xl'>Contact</li>
+          <li className='py-6 text-4xl'>
+            {' '}
+            <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+              Home
+            </Link></li>
+          <li className='py-6 text-4xl'>
+            {' '}
+            <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+                About
+            </Link></li>
+          <li className='py-6 text-4xl'>
+            {' '}
+            <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+                Skills
+          </Link></li>
+          <li className='py-6 text-4xl'>
+            {' '}
+            <Link onClick={handleClick} to="projects" smooth={true} duration={500}>
+              Projects
+            </Link></li>
+          <li className='py-6 text-4xl'>
+            {' '}
+            <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+              Contact Me
+          </Link></li>
         </ul>
          {/* Social media Icons */}
          <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
               <ul>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-800'>
                   <a className='flex justify-between items-center w-full text-gray-300'
-                  href="/">
+                  href="https://www.linkedin.com/in/okikiojikutu/" target='_blank'>
                     Linkedin <FaLinkedin size={30}/>
                   </a>
                 </li>
@@ -48,7 +89,7 @@ const Navbar = () => {
                 </li>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#ffff]'>
                   <a className='flex justify-between items-center w-full text-gray-900'
-                  href="/">
+                  href="/contact">
                     Mail <HiOutlineMail size={30}/>
                   </a>
                 </li>
