500823e62a30539b0717df9d9d41d5a44a97e120
Okikiola Ojikutu <okikiolaojikutue@gmail.com>
2023-08-21 20:24:26 -0600

    updates to navbar and css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, {useState}from 'react'
+import {FaBars, FaTimes} from 'react-icons/fa'
+import Logo from '../assets/logo.png'
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false)
+  const handleClick = () => setNav(!nav)
+  
   return (
-    <div className= 'fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]'>
-      
+    <div className= 'fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+      <div>
+        <img src={Logo} alt="Logo Image" style={{width: '135px'}}/>
+      </div>
+        <ul className='hidden md:flex'>
+          <li>Home</li>
+          <li>About</li>
+          <li>Experience</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      <div onClick={handleClick} className='md:hidden z-10'>
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+      {/* mobile menu */}
+      <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+          <li className='py-6 text-4xl'>Home</li>
+          <li className='py-6 text-4xl'>About</li>
+          <li className='py-6 text-4xl'>Experience</li>
+          <li className='py-6 text-4xl'>Projects</li>
+          <li className='py-6 text-4xl'>Contact</li>
+        </ul>
+         {/* Social media Icons */}
+         <div className='hidden'></div>
     </div>
   )
 }
